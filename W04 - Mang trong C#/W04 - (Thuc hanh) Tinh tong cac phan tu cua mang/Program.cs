c3374109f1ffbb743ba8f184de4124dea05b570d
kioop456 <tunguyen20032000@gmail.com>
2024-03-02 19:11:11 +0700

    update

---
+++ W04 - Mang trong C#/W04 - (Thuc hanh) Tinh tong cac phan tu cua mang/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W04____Thuc_hanh__Tinh_tong_cac_phan_tu_cua_mang
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[5];
+
+            numbers[0] = 2;
+            numbers[1] = 5;
+            numbers[2] = 9;
+            numbers[3] = 6;
+            numbers[4] = 7;
+
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
+            Console.WriteLine(numbers[3]);
+
+            int total = 0;
+            for(int i = 0; i < numbers.Length; i++ )
+            {
+                total = total + numbers[i];
+            }
+            Console.WriteLine("the total is: " + total);
+
+
+            Console.ReadKey();
+        }
+    }
+}
